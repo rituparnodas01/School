7c5a1830f7454cdda9d8b30ef055c243abdfd818
Rituparno Das <rituparnodas.datacoresystems@gmail.com>
2024-04-24 17:00:33 +0530

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const router = require('./routes/admin');
 const port = process.env.PORT || 4000;
-// require("./src/models/index")
 app.use(express.json());
 app.use(cors());
 
@@ -11,4 +10,4 @@ app.use("/tffs", router);
 
 app.listen(port, () => {
     console.log(`Server is running at port no ${port}`);
-});
+}); 
